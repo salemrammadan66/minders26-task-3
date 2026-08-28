5c2adb07e755d7371625010c42fb0c0f4b3fbf1d
Salem Ramadan <salemramdan12345@gmail.com>
2026-08-28 22:20:46 +0300

    Initial commit + app colors added

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff6C63FF);
+  static const Color secondaryColor = Color(0xff081854);
+  static const Color backgoundColor = Color(0xffF4F3FF);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color sliderColor = Color(0xffCCCCCC);
+  static const Color normalBMIColor = Color(0xff34C759);
+  static const Color underWeightColor = Color(0xff00C8B3);
+  static const Color overWeightIColor = Color(0xffFF8D28);
+  static const Color obesityColor = Color(0xffFF383C);
+}
